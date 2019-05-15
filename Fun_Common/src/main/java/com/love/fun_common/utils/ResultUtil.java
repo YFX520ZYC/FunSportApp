65b262c9d63376a34b600fd4aa5007c7f32b05da
YFX520ZYC <1668997610@qq.com>
2019-05-15 10:26:14 +0800

    5/15号  公共配置

---
+++ Fun_Common/src/main/java/com/love/fun_common/utils/ResultUtil.java
@@ -0,0 +1,26 @@
+package com.love.fun_common.utils;
+
+
+import com.love.fun_common.constsys.SystemConst;
+import com.love.fun_common.vo.ResultVO;
+
+/**
+ *@Author feri
+ *@Date Created in 2019/4/28 17:28
+ */
+public class ResultUtil {
+    public static ResultVO exec(boolean istrue, String msg, Object obj){
+        ResultVO resultVO=new ResultVO(istrue?SystemConst.OK: SystemConst.ERROR,msg,obj);
+        return resultVO;
+    }
+    public static ResultVO setOK(){
+        return exec(true,"OK",null);
+    }
+    public static ResultVO setERROR(){
+        return exec(false,"ERROR",null);
+    }
+    public static ResultVO setOK(String msg){
+        return exec(true,msg,null);
+    }
+
+}
